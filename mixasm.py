fb08212cca065f457d14e646b6f3e33ca4151678
1nzag <wkdwns420@gmail.com>
2018-05-06 03:54:36 +0900

    parse asm and substitution

--- mixasm.py
+++ mixasm.py
@@ -1,7 +1,7 @@
 import os, sys
 from pwn import *
 from parsecode import elfinfo
-
+import random
 
 class parseasm:
 	def __init__(self, stream, arch_code):
@@ -12,6 +12,7 @@ class parseasm:
 			context.arch = 'amd64'
 		
 		self.asm = []
+		self.stream = stream
 		for line in disasm(stream).split('\n'):
 			inst = {}
 			target = line.split(' ')
@@ -42,15 +43,42 @@ class parseasm:
 			inst['inst'] = inst_string
 			self.asm.append(inst)
 	
+	def nop_padding(self, inst, newcode):
+		if len(new_code) < inst['size']:
+			rand = random.randint(0, size)
+			return ("\x90" * rand) + inst + ("\x90" * (size - rand))
+		elif len(new_code) > inst['size']:
+			return False
+		else:
+			return new_code
+	
 	def substitution(self):
-					
-				
+		for inst in self.inst:
+			if "mov" in inst['inst']:
+				#mov
+				code = inst['inst'].split('mov')[1].replace(" ", "")
+				src = code.split(",")[1]
+				dest = code.split(",")[0]  #mov dest, src
+				if "PTR" in src:
+					continue  #not serviced
+				elif "l" in dest:
+					continue  #not serviced
+				elif "x" in dest:
+					if int(src, 16) < 256:
+						new_code = asm("mov " + src[1:].replace("x", "l") + ", " + src) # if extension register
+						if self.nop_padding(inst, new_code) !=  False:
+							self.stream = self.stream[:inst['offset']] + new_code + self.stream[inst['offset'] + inst['size']:]
+				else:
+					continue # not serviced
+	def result(self):
+		return self.stream
+							
 			
 
 if __name__ == '__main__':
 	stream = open(sys.argv[1], "rb").read()
 	point = elfinfo(sys.argv[1]).code_area()
-	parseasm(stream[point['offset']:point['offset'] + point['size']], 64)
+	#print parseasm(stream[point['offset']:point['offset'] + point['size']], 64).substitution()
 
 
 			
